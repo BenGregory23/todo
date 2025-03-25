5baf8e7746eb1038b956b8971bbe331259665af3
Ben Gregory <bengregorygbr@gmail.com>
2025-03-25 11:02:01 +0100

    first commit

---
+++ pages/index.vue
@@ -0,0 +1,21 @@
+<script setup lang="ts">
+import PocketBase from 'pocketbase';
+
+const pb = new PocketBase('https://wind.oxyjen.io');
+
+
+onMounted(async () =>{
+  const records = await pb.collection('tasks').getFullList({
+    sort: '-someField',
+  });
+
+  console.log(records);
+})
+
+
+</script>
+
+<template>
+  <Button>Test</Button>
+</template>
+
